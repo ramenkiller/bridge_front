5321737341fd000aa788167567af7f331cc74c42
ramenkiller <162812787+ramenkiller@users.noreply.github.com>
2024-05-14 18:39:00 +0900

    add /write

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import  {Button, Form} from 'react-bootstrap';
 
 const Write = () =>{
     return(
-        <div>
-            <h1>글쓰기</h1>
-        </div>
+        <Form> 
+            <Form.Group className="mb-3"> 
+                <Form.Label>제목</Form.Label> 
+                <Form.Control /> 
+            </Form.Group> 
+            <Form.Group className="mb-3"> 
+                <Form.Label>내용</Form.Label> 
+                <Form.Control as="textarea" rows={3} /> 
+            </Form.Group> 
+            <Button>글쓰기</Button>
+        </Form>
     );
 };
 
