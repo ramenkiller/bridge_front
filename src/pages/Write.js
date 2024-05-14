2fa8cb84d51effb576dac6f4c4c2f87f76288599
ramenkiller <162812787+ramenkiller@users.noreply.github.com>
2024-05-14 19:17:03 +0900

    add post

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import  {Button, Form} from 'react-bootstrap';
 
 const Write = () =>{
+    const [post, setPost] = useState();
+
+    const changeValue = (e) => {
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value,
+        });
+        console.log(post);
+    };
+
+    const submitPost = (e) =>{
+        e.preventDefault();
+        fetch('http://localhost:8090/test', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset-utf-8',
+            },
+            body: JSON.stringify(post),
+        })
+            .then((res) => res.json())
+            .then((res) => console.log(res));
+    }
+
     return(
-        <Form> 
+        <Form onSubmit={submitPost}> 
             <Form.Group className="mb-3"> 
                 <Form.Label>제목</Form.Label> 
-                <Form.Control /> 
+                <Form.Control name="title" onChange={changeValue} /> 
             </Form.Group> 
             <Form.Group className="mb-3"> 
                 <Form.Label>내용</Form.Label> 
-                <Form.Control as="textarea" rows={3} /> 
+                <Form.Control as="textarea" rows={3} name="contents" onChange={changeValue} /> 
             </Form.Group> 
-            <Button>글쓰기</Button>
+            <Button type="submit">글쓰기</Button>
         </Form>
     );
 };
